ae07b40f7349b35caf6a9d290189e861433dc350
Agustin Palma <palma.agustin7@gmail.com>
2021-06-29 13:12:10 -0400

    added css to fixed nav bar in all the page

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,28 @@
 document.addEventListener("DOMContentLoaded", function() {
     scroll_nav();
+
+    navegacion_fija();
 })
+
+// function navegacion_fija() {
+
+//     const barra = document.querySelector(".header");
+//     //registrando el intersection observer
+//     const observer = new IntersectionObserver(function(entries) {
+//         if (entries[0].isIntersecting) {
+//             barra.classList.remove("fijo");
+//             console.log(entries[0]);
+//         } else {
+//             barra.classList.add("fijo");
+//             console.log(entries[0]);
+//         }
+//     });
+
+
+
+//     // registrando el elemento a observar
+//     observer.observe(document.querySelector(".video"));
+// }
 
 function scroll_nav() {
     const enlaces = document.querySelectorAll(".navegacion-principal a");
